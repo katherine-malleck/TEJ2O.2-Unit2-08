94aa1885d47a13f7915c58241f67777d6b50e258
katherine <katherine.malleck@stu.ocsb.ca>
2026-03-11 12:10:17 -0400

    let

--- main.ts
+++ main.ts
@@ -1,8 +1,60 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Katherine
+ * Created on: Mar 2026
+ * This program will change colours
 */
 
-basic.showString('Hello, World!')
+let red = DigitalPin.P0
+let green = DigitalPin.P1
+let blue = DigitalPin.P2
+
+input.onButtonPressed(Button.A, function () {
+
+    // red
+    pins.digitalWritePin(red, 1)
+    pins.digitalWritePin(green, 0)
+    pins.digitalWritePin(blue, 0)
+    basic.pause(1000)
+
+    // green
+    pins.digitalWritePin(red, 0)
+    pins.digitalWritePin(green, 1)
+    pins.digitalWritePin(blue, 0)
+    basic.pause(1000)
+
+    // Blue
+    pins.digitalWritePin(red, 0)
+    pins.digitalWritePin(green, 0)
+    pins.digitalWritePin(blue, 1)
+    basic.pause(1000)
+
+    // Yellow
+    pins.digitalWritePin(red, 1)
+    pins.digitalWritePin(green, 1)
+    pins.digitalWritePin(blue, 0)
+    basic.pause(1000)
+
+    // Cyan
+    pins.digitalWritePin(red, 0)
+    pins.digitalWritePin(green, 1)
+    pins.digitalWritePin(blue, 1)
+    basic.pause(1000)
+
+    // Magenta
+    pins.digitalWritePin(red, 1)
+    pins.digitalWritePin(green, 0)
+    pins.digitalWritePin(blue, 1)
+    basic.pause(1000)
+
+    // White
+    pins.digitalWritePin(red, 1)
+    pins.digitalWritePin(green, 1)
+    pins.digitalWritePin(blue, 1)
+    basic.pause(1000)
+
+    // Turn off LED
+    pins.digitalWritePin(red, 0)
+    pins.digitalWritePin(green, 0)
+    pins.digitalWritePin(blue, 0)
+})
